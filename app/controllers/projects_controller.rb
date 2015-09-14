d7a8d7e1fd81d2547de84fce2a5382943beec7f5
May Miller-Ricci <maymillerricci@gmail.com>
2015-09-14 00:11:44 -0700

    add projects controller

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,10 @@
+class ProjectsController < ApplicationController
+  def canvas_draw
+  end
+
+  def periodic_table
+  end
+
+  def countries
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'projects/canvas_draw'
+
+  get 'projects/periodic_table'
+
+  get 'projects/countries'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
