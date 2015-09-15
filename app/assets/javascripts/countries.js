052e106a24bbc9ce4131b43fc4b08cc758a0c3cb
May Miller-Ricci <maymillerricci@gmail.com>
2015-09-15 00:51:23 -0700

    fix indentation

--- app/assets/javascripts/countries.js
+++ app/assets/javascripts/countries.js
@@ -1,19 +1,19 @@
 //what happens when country form is submitted
 $(document).on('submit', '#country_form', function() {
   $.post(
-      $(this).attr('action'),
-      $(this).serialize(),
-      function(data) {
-        $('#country_info').html(
-            "<h2>Country Information</h2>" +
-            "<img src='" + data.asset_path + "' alt='" + data.name + " flag'>" +
-            "<p>Country: " + data.name + "</p>" +
-            "<p>Continent: " + data.continent + "</p>" +
-            "<p>Region: " + data.subregion + "</p>" +
-            "<p>Nationality: " + data.nationality + "</p>" +
-            "<p>Currency: " + data.currency + "</p>");
-      },
-      "json"
+    $(this).attr('action'),
+    $(this).serialize(),
+    function(data) {
+      $('#country_info').html(
+        "<h2>Country Information</h2>" +
+        "<img src='" + data.asset_path + "' alt='" + data.name + " flag'>" +
+        "<p>Country: " + data.name + "</p>" +
+        "<p>Continent: " + data.continent + "</p>" +
+        "<p>Region: " + data.subregion + "</p>" +
+        "<p>Nationality: " + data.nationality + "</p>" +
+        "<p>Currency: " + data.currency + "</p>");
+    },
+    "json"
   );
   return false;
 });
