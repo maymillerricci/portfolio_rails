3525f30e0f4f93b4d9fba4f5fe5ec07abe394aa8
May Miller-Ricci <maymillerricci@gmail.com>
2015-09-15 00:24:44 -0700

    add flag asset_path to ajax data

--- app/assets/javascripts/countries.js
+++ app/assets/javascripts/countries.js
@@ -4,11 +4,11 @@ $(document).on('submit', '#country_form', function() {
       $(this).attr('action'),
       $(this).serialize(),
       function(data) {
-        var unknown = "this.src=''";
-        // var unknown = "this.src='assets/images/flags/unknown.png'";
+        //var unknown = "this.src=''";
         $('#country_info').html(
             "<h2>Country Information</h2>" +
-            //"<img src='assets/images/flags/" + data['country_info']['name'] + ".png' onerror=" + unknown + ">" +
+            //"<img src='/app/assets/images/flags/" + data.name + ".png' onerror=" + unknown + ">" +
+            "<img src='" + data.asset_path + "'>" +
             "<p>Country: " + data.name + "</p>" +
             "<p>Continent: " + data.continent + "</p>" +
             "<p>Region: " + data.subregion + "</p>" +
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -13,6 +13,7 @@ class ProjectsController < ApplicationController
 
   def country_data
     @country = ISO3166::Country.find_country_by_name(params[:country]).data
+    @country["asset_path"] = ActionController::Base.helpers.asset_path("flags/#{@country["name"]}.png")
     respond_to do |format|
       format.json { render json: @country }
     end
